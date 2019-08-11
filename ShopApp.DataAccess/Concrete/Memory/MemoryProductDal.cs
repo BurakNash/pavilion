ab74cbf173a99dce75ed6ea6bdbe98f19c482d91
Burak Koc <burakkayakoc@gmail.com>
2019-08-11 16:39:05 -0500

    Memory folder crated, MemoryProductDal class created

---
+++ ShopApp.DataAccess/Concrete/Memory/MemoryProductDal.cs
@@ -0,0 +1,47 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.Memory
+{
+    public class MemoryProductDal : IProductDal
+    {
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetOne(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetPopularProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
