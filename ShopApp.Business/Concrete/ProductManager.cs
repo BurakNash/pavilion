3ed5a78513bd6d92581ac29a34cea65fa7c94566
Burak Koc <burakkayakoc@gmail.com>
2019-08-11 16:02:18 -0500

    IProductDal is connected to ProductManager

---
+++ ShopApp.Business/Concrete/ProductManager.cs
@@ -0,0 +1,42 @@
+﻿using ShopApp.Business.Abstract;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal; //Accesssing ProductDal and gettting the functions
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal; //connecting objects 
+        }
+        public void Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
