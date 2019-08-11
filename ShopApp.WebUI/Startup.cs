a138f803f4d5d2036c3d1df544de0c9d20c091c3
Burak Koc <burakkayakoc@gmail.com>
2019-08-11 16:56:35 -0500

    AddScoped added on Startup

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.DataAccess.Concrete.Memory;
 
 namespace ShopApp.WebUI
 {
@@ -15,6 +17,7 @@ namespace ShopApp.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductDal, MemoryProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
