f563056a5626bc490da1d64abd81451610b37b9a
Burak Koc <burakkayakoc@gmail.com>
2019-08-13 14:31:14 -0500

    Conditionals completed, Context save changes

--- ShopApp.DataAccess/Concrete/EfCore/SeedDatabase.cs
+++ ShopApp.DataAccess/Concrete/EfCore/SeedDatabase.cs
@@ -24,7 +24,7 @@ namespace ShopApp.DataAccess.Concrete.EfCore
                 {
                     context.Products.AddRange(Products);
                 }
-                
+                context.SaveChanges();
             }
         }
         private static Product[] Products =
