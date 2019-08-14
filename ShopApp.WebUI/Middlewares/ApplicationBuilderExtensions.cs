c53cb739e8546b6724e3cdbb5c4157959a2342f3
Burak Koc <burakkayakoc@gmail.com>
2019-08-13 20:18:19 -0500

    Static class IApplicationBuilder

---
+++ ShopApp.WebUI/Middlewares/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Middlewares
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+            //"this" extends the next class in the parameter list
+            //Subclass system, study this
+        {
+
+        }
+    }
+}
