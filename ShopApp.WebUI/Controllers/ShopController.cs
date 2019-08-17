02d399f431ddd91ff7733be22304a616ca04cd87
Burak Koc <burakkayakoc@gmail.com>
2019-08-17 13:02:47 -0500

    Details controller added

--- ShopApp.WebUI/Controllers/ShopController.cs
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ShopApp.Business.Abstract;
+using ShopApp.Entities;
 using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
@@ -15,9 +16,18 @@ namespace ShopApp.WebUI.Controllers
         {
             _productService = productService;
         }
-        public IActionResult Index()
+        public IActionResult Details(int? id) //Nullable
         {
-            return View();
+            if (id==null)
+            {
+                return NotFound();
+            }
+            Product product = _productService.GetById((int)id); //converting to int
+            if (product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
         public IActionResult List()
         {
