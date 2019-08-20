c27d9610558c59d31f5b667abfc5a6d88c490ea1
Burak Koc <burakkayakoc@gmail.com>
2019-08-20 10:50:39 -0500

    PageLinkTagHelper folder added, TagHelper extension added (study)

---
+++ ShopApp.WebUI/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.TagHelpers
+{
+    [HtmlTargetElement("div",Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+    }
+}
