1c7236a19dc8a27b933f29ac2495ff083bf9e5ac
Burak Koc <burakkayakoc@gmail.com>
2019-08-20 09:43:31 -0500

    Math.Ceiling((decimal)TotalItems / ItemsPerPage)

--- ShopApp.WebUI/Models/ProductListModel.cs
+++ ShopApp.WebUI/Models/ProductListModel.cs
@@ -11,6 +11,11 @@ namespace ShopApp.WebUI.Models
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
+        public string CurrentCategory { get; set; }
+        public int TotalPages()
+        {
+            return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+        }
     }
     public class ProductListModel
     {
