ee5c51d1d8c6b1f9b4ac48d2b30702e3aa1d83a3
Burak Koc <burakkayakoc@gmail.com>
2019-08-20 10:58:18 -0500

    PageInfo added to TageHelper

--- ShopApp.WebUI/TagHelpers/PageLinkTagHelper.cs
+++ ShopApp.WebUI/TagHelpers/PageLinkTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using ShopApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,8 @@ namespace ShopApp.WebUI.TagHelpers
     [HtmlTargetElement("div",Attributes = "page-model")]
     public class PageLinkTagHelper : TagHelper
     {
+        public PageInfo PageInfo { get; set; }
+
+
     }
 }
