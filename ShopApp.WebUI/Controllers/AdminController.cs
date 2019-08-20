242a2b57bbf85dd8a81a59b9d9832c1287566245
Burak Koc <burakkayakoc@gmail.com>
2019-08-20 14:37:35 -0500

    Input for product name created

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -21,7 +21,7 @@ namespace ShopApp.WebUI.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateProduct(Product entity)
+        public IActionResult CreateProduct(Product entity)//entities bringing the product information, no need to write eachtime
         {
             return Redirect("index");
         }
