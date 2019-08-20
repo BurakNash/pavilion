c76f64f771019c03145ad6aabaf7b2349bfc9de5
Burak Koc <burakkayakoc@gmail.com>
2019-08-20 11:02:31 -0500

    StringBuilder added (study)

--- ShopApp.WebUI/TagHelpers/PageLinkTagHelper.cs
+++ ShopApp.WebUI/TagHelpers/PageLinkTagHelper.cs
@@ -3,6 +3,7 @@ using ShopApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopApp.WebUI.TagHelpers
@@ -12,6 +13,19 @@ namespace ShopApp.WebUI.TagHelpers
     {
         public PageInfo PageInfo { get; set; }
 
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
 
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("<ul class='pagination'>");
+
+            for (int i = 1; i < PageInfo.TotalPages(); i++)
+            {
+
+            }
+
+            base.Process(context, output);
+        }
     }
 }
