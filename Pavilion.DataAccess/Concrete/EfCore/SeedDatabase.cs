f5a9b3c85fe69e9bd88ec8575631684ac5935599
Burak Koc <burakkayakoc@gmail.com>
2019-09-10 10:09:43 -0500

    edits

--- Pavilion.DataAccess/Concrete/EfCore/SeedDatabase.cs
+++ Pavilion.DataAccess/Concrete/EfCore/SeedDatabase.cs
@@ -31,20 +31,20 @@ namespace Pavilion.DataAccess.Concrete.EfCore
         }
 
         private static Category[] Categories = {
-            new Category() { Name="Telefon"},
-            new Category() { Name="Bilgisayar"},
-            new Category() { Name="Elektronik"}
+            new Category() { Name="Phone"},
+            new Category() { Name="Computer"},
+            new Category() { Name="Electronics"}
         };
 
         private static Product[] Products =
         {
-            new Product(){ Name="Samsung S5", Price=2000, ImageUrl="1.jpg", Description="<p>güzel telefon</p>"},
-            new Product(){ Name="Samsung S6", Price=3000, ImageUrl="2.jpg", Description="<p>güzel telefon</p>"},
-            new Product(){ Name="Samsung S7", Price=4000, ImageUrl="3.jpg", Description="<p>güzel telefon</p>"},
-            new Product(){ Name="Samsung S8", Price=5000, ImageUrl="4.jpg", Description="<p>güzel telefon</p>"},
-            new Product(){ Name="Samsung S9", Price=6000, ImageUrl="5.jpg", Description="<p>güzel telefon</p>"},
-            new Product(){ Name="IPhone 6", Price=4000, ImageUrl="6.jpg", Description="<p>güzel telefon</p>"},
-            new Product(){ Name="IPhone 7", Price=5000, ImageUrl="7.jpg", Description="<p>güzel telefon</p>"}
+            new Product(){ Name="Samsung S5", Price=2000, ImageUrl="1.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Samsung S6", Price=3000, ImageUrl="2.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Samsung S7", Price=4000, ImageUrl="3.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Samsung S8", Price=5000, ImageUrl="4.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Samsung S9", Price=6000, ImageUrl="5.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="IPhone 6", Price=4000, ImageUrl="6.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="IPhone 7", Price=5000, ImageUrl="7.jpg", Description="<p>Description here</p>"}
         };
 
 
