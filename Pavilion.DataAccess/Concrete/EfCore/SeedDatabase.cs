d315080aa997cb5497103646675e0c6bb93c3071
Burak Koc <burakkayakoc@gmail.com>
2019-09-12 14:23:41 -0500

    seed data changed

--- Pavilion.DataAccess/Concrete/EfCore/SeedDatabase.cs
+++ Pavilion.DataAccess/Concrete/EfCore/SeedDatabase.cs
@@ -38,25 +38,25 @@ namespace Pavilion.DataAccess.Concrete.EfCore
 
         private static Product[] Products =
         {
-            new Product(){ Name="Samsung S5", Price=2000, ImageUrl="1.jpg", Description="<p>Description here</p>"},
-            new Product(){ Name="Samsung S6", Price=3000, ImageUrl="2.jpg", Description="<p>Description here</p>"},
-            new Product(){ Name="Samsung S7", Price=4000, ImageUrl="3.jpg", Description="<p>Description here</p>"},
-            new Product(){ Name="Samsung S8", Price=5000, ImageUrl="4.jpg", Description="<p>Description here</p>"},
-            new Product(){ Name="Samsung S9", Price=6000, ImageUrl="5.jpg", Description="<p>Description here</p>"},
-            new Product(){ Name="IPhone 6", Price=4000, ImageUrl="6.jpg", Description="<p>Description here</p>"},
-            new Product(){ Name="IPhone 7", Price=5000, ImageUrl="7.jpg", Description="<p>Description here</p>"}
+            new Product(){ Name="Ring 1", Price=10, ImageUrl="1.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Ring 2", Price=20, ImageUrl="2.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Ring 3", Price=30, ImageUrl="3.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Necklace 1", Price=40, ImageUrl="4.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Necklace 2", Price=50, ImageUrl="5.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Necklace 3", Price=60, ImageUrl="6.jpg", Description="<p>Description here</p>"},
+            new Product(){ Name="Earring 1", Price=70, ImageUrl="7.jpg", Description="<p>Description here</p>"}
         };
 
 
         private static ProductCategory[] ProductCategory =
         {
-            new ProductCategory() { Product= Products[0],Category= Categories[0]},
-            new ProductCategory() { Product= Products[0],Category= Categories[2]},
-            new ProductCategory() { Product= Products[1],Category= Categories[0]},
             new ProductCategory() { Product= Products[1],Category= Categories[1]},
-            new ProductCategory() { Product= Products[2],Category= Categories[0]},
-            new ProductCategory() { Product= Products[2],Category= Categories[2]},
-            new ProductCategory() { Product= Products[3],Category= Categories[1]}
+            new ProductCategory() { Product= Products[2],Category= Categories[1]},
+            new ProductCategory() { Product= Products[3],Category= Categories[1]},
+            new ProductCategory() { Product= Products[4],Category= Categories[2]},
+            new ProductCategory() { Product= Products[5],Category= Categories[2]},
+            new ProductCategory() { Product= Products[6],Category= Categories[2]},
+            new ProductCategory() { Product= Products[7],Category= Categories[3]}
         };
     }
 }
