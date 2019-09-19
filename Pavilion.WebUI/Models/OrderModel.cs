90b945a0d62224ae880a4d2128adaca11e409976
Burak Koc <burakkayakoc@gmail.com>
2019-09-18 20:58:06 -0500

    model edit

--- Pavilion.WebUI/Models/OrderModel.cs
+++ Pavilion.WebUI/Models/OrderModel.cs
@@ -27,7 +27,6 @@ namespace Pavilion.WebUI.Models
         public string ExpirationYear { get; set; }
         public string Cvv { get; set; }
         public CartModel CartModel { get; set; }
-        public int CartId { get; set; }
        
     }
 }
